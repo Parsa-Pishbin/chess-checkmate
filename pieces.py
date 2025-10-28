7992ea7c5d7282a1ec4c02413c46c4c79ef1bb42
Parsa <m.p.pishbin@gmail.com>
2025-10-28 20:42:37 +0330

    create get_actions and calculate_action in Piece

--- pieces.py
+++ pieces.py
@@ -4,7 +4,8 @@ BLACK = False
 class Piece:
     row:int = None
     index:int = None
-    directions:list[tuple] = []
+    directions:list[tuple[int, int]] = []
+    board = None
 
     def __init__(self, color:bool):
         self.color = color
@@ -15,4 +16,28 @@ class Piece:
     @property
     def location(self):
         return (self.row, self.index)
+    
+    def get_actions(self):
+        if self.row is None or self.index is None or self.board is None:
+            raise ValueError('the piece must be on a board')
+
+        result = set()
+
+        for direction in self.directions:
+            action = self.calculate_action(direction)
+            if action is not None:
+                result.add(action)
+
+        return result
+    
+    def calculate_action(self, direction):
+        dr, di = direction
+        if not self.color:
+            dr *= -1
+        new_row = dr + self.row
+        new_index = di + self.index
+
+        if -1 < new_row < 8 and -1 < new_index < 8:
+            return (new_row, new_index)
+
     
